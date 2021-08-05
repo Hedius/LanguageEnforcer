69c2ec29dc7a7c048dd6f9d947f52819037cd228
Hedius <git@hedius.eu>
2021-08-05 23:33:09 +0200

    fix log msg

--- LanguageEnforcer.cs
+++ LanguageEnforcer.cs
@@ -1535,7 +1535,7 @@ namespace PRoConEvents
 
 		public void LogViolation(String player, String message, String match) 
 		{
-			Log(player, $"Language Violation: Word: {match} Message: {message}");
+			Log(player, $"Violation: Match: {match} Message: {message}");
 		}
 
 		public void Say(string message)
